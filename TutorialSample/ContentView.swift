7318485b9e42ad0b163e106a230d833ff97d87cd
riiamri <riiamri23@gmail.com>
2023-11-20 21:23:55 +0700

    Initial Commit

---
+++ TutorialSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TutorialSample
+//
+//  Created by rii amri on 20/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TutorialSample/TutorialSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialSampleApp.swift
+//  TutorialSample
+//
+//  Created by rii amri on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TutorialSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
